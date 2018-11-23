fa03773087d775152bfaf0ee801c032518abaef1
ignaciosmix <43443976+ignaciosmix@users.noreply.github.com>
2018-11-23 22:53:28 +0100

    Update logout.php

--- pages/logout.php
+++ pages/logout.php
@@ -2,24 +2,18 @@
     
      //Borrado de cookies al hacer el logout
      
-if(isset($_COOKIE['cookielogin']) || isset($_COOKIE['cookiepass']) ) {
-    if(isset($_COOKIE['lasthourvisited']) || isset($_COOKIE['lastdayvisited']) ) {
-
-     unset($_COOKIE['cookielogin']);
-     unset($_COOKIE['cookiepass']);
-     unset($_COOKIE['lasthourvisited']);
-     unset($_COOKIE['lastdayvisited']);
-
-
      setcookie('cookielogin',null, time()-3600, '/');
      setcookie('cookiepass',null, time()-3600, '/');
      setcookie('lasthourvisited',null, time()-3600, '/');
      setcookie('lastdayvisited',null, time()-3600, '/');
       
-
-     
-    }
-}
+    
+     //USADO PARA PRUEBAS CON IFSET
+     /*unset($_COOKIE['cookielogin']);
+     unset($_COOKIE['cookiepass']);
+     unset($_COOKIE['lasthourvisited']);
+     unset($_COOKIE['lastdayvisited']);
+    */
 // Borra todas las variables de sesión 
      
      session_destroy();
